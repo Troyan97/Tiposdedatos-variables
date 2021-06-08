13117fc36209e993ef5de640a185e380f21a87b8
Troyan97 <83074767+Troyan97@users.noreply.github.com>
2021-06-08 10:47:54 -0300

    Add files via upload

---
+++ ejercicio 4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/*
+Pocion multijugos
+Modifique a la cantidad correspondiente los primeros 5 ingredientes SOLO en las instrucciones printf
+    
+Para más informacion visitar: https://harrypotter.fandom.com/es/wiki/Poci%C3%B3n_multijugos#Ingredientes
+*/
+
+
+int main(){
+    //NO MODIIFICAR
+    int cantidadCuatro = 4;
+    int cantidadUno = 1;
+    int cantidadDos = 2;
+    int cantidadDieciseis = 16;
+    int cantidadDoce = 12;
+    //NO MODIIFICAR
+
+    printf("Ingredientes para la pocion multijugos:\n");
+    printf("- %i Crisopos que se han guisado durante 21 dias\n", cantidadDoce);
+    printf("- %i Onza de antimonio crudo\n- %i sanguijuelas\n", cantidadUno, cantidadCuatro);
+    printf("- %i escrupulos de Descurainia sophia recogida con luna llena \n", cantidadDieciseis);
+    printf("- %i hojas pulversadas de centinodia\n", cantidadDos);
+    printf("- Un poco de cuerno pulverizado de Bicornio, extraida segun ciertas condiciones de la luna\n");
+    printf("- Piel seca desmenuzada de una serpiente arborea africana\n");
+    printf("- Gusarajo\n");
+    printf("- Algo de la persona en la que se quiere transformar\n");
+
+    return 0;
+}
